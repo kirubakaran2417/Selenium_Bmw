0aa2d17cfba4d9a19ab143957f33b4406cc05610
hello.gmail.com <nisha>
2023-08-29 17:40:33 +0530

    initial commit

--- src/test/java/org/example/testcases/Testcases.java
+++ src/test/java/org/example/testcases/Testcases.java
@@ -3,9 +3,13 @@ package org.example.testcases;
 import org.example.base.BrowserConfiguration;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class Testcases {
     public WebDriver driver;
@@ -14,7 +18,7 @@ public class Testcases {
     public void setup(){//whenever a method is annotated with @Test, it is a test case,we are testng framework
         driver = BrowserConfiguration.getChromeBrowser();
     }
-    @Test
+    @Test(priority = 0)
     public void BrowserCommandsandMethods(){
         driver.get("https://www.mycontactform.com");
 //        driver.navigate().to("http://www.google.com");//another way to navigate to webpage
@@ -25,7 +29,7 @@ public class Testcases {
         System.out.println(driver.getTitle());
         System.out.println(driver.getCurrentUrl());
     }
-    @Test
+    @Test(priority = 1)
     public void locators(){
     //using linktext
     driver.findElement(By.linkText("Sample Forms")).click();
@@ -33,6 +37,28 @@ public class Testcases {
     driver.findElement(By.id("subject")).sendKeys("AZOLA");
     driver.findElement(By.name("q5")).click();
     driver.findElement(By.xpath("//input[@name='email_to[]'][@value='1']")).click();
+
+    WebElement ele=driver.findElement(By.xpath("//input[@id='q4'][@value='Third Option']"));
+    ele.click();
+    //how to handle dropdown
+    WebElement ele1=driver.findElement(By.id("q3"));
+        Select s=new Select(ele1);
+        s.selectByVisibleText("Second Option");
+    List<WebElement> checkboxes=driver.findElements(By.xpath("//input[@type='checkbox']"));
+    System.out.println("Total number of checkboxes: "+checkboxes.size());
+}
+
+@Test(priority = 2)
+public void findelementss(){
+    //find how many links in this page
+    driver.navigate().to("https://www.bmw.in/en/index.html");
+    List<WebElement> links=driver.findElements(By.tagName("a"));
+    System.out.println("Total number of links: "+links.size());
+    //print all the href values
+    for(int i=0;i<links.size();i++){
+        System.out.println(links.get(i).getAttribute("href"));
+    }
+
 }
 //    @AfterTest
 //    public void tearDown(){
